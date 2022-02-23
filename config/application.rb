f4926c5b71a03636ebf22fa535fb0133c5585517
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2022-02-23 03:35:55 +0000

    Configurando Rspec

--- config/application.rb
+++ config/application.rb
@@ -28,13 +28,13 @@ module UFFMailService
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
-
+    config.generators.system_tests = nil
     config.generators do |g|
       g.test_framework :rspec,
         fixtures :false,
         helper_specs :false,
         routing_specs :false
-
+    end
     config.api_only = true
   end
 end
