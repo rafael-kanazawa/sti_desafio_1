b01f106275e2e5fcc1ecd44bd94ad90a904f1367
Rafael Kanazawa <rafael_kanazawa@id.uff.br>
2022-02-23 01:16:48 +0000

    Gerando migracao para tabela de alunos

---
+++ db/migrate/20220223004501_create_students.rb
@@ -0,0 +1,14 @@
+class CreateStudents < ActiveRecord::Migration[5.2]
+  def change
+    create_table :students do |t|
+      t.string :nome
+      t.string :matricula
+      t.string :telefone
+      t.string :email
+      t.string :uffmail
+      t.boolean :status
+
+      t.timestamps
+    end
+  end
+end
